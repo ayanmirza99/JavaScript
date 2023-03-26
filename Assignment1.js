778afa4024334bdf02e1eb5c3e6d8e4b69defa0f
ayanmirza99 <125931448+ayanmirza99@users.noreply.github.com>
2023-03-26 22:01:39 +0500

    Create Assignment1.js

---
+++ Assignment1.js
@@ -0,0 +1,7 @@
+// Declare a variable called “age” and assign it a value of 25.
+// Convert the value of age to a string data type and store it in a new variable called “ageAsString”.
+let age=25;
+console.log(typeof age)
+let ageAsString=String(age);
+console.log(ageAsString)
+console.log(typeof ageAsString)
