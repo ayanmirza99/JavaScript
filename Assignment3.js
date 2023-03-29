7cc429c2471efc955ecadbea339539ecca8f8cbb
ayanmirza99 <125931448+ayanmirza99@users.noreply.github.com>
2023-03-29 06:04:16 +0500

    Create Assignment3.js

---
+++ Assignment3.js
@@ -0,0 +1,5 @@
+// Declare a variable called “temperature” and assign it a value of 72.
+// Convert the temperature to Celsius and store the result in a new variable called “celsius”. Display the value of celsius to the console.
+const temperature=72;
+const celsius = (temperature - 32) / 1.8;
+console.log(`${temperature} degree celsius is equal to ${celsius} degree fahrenheit.`);
