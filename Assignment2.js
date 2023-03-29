40dba3da2965cdc32ff8d87500d2fbc8751c7414
ayanmirza99 <125931448+ayanmirza99@users.noreply.github.com>
2023-03-29 06:21:23 +0500

    Create Assignment2.js

---
+++ Assignment2.js
@@ -0,0 +1,9 @@
+// Create a program that prompts the user to enter a number and then checks whether that number is even or odd.
+// Display the result to the user using an alert box.
+let num = prompt("Enter your Number:");
+if(num%2==0){
+    alert(`${num} is an even number.`)
+}
+else{
+    alert(`${num} is an odd number.`)
+}
